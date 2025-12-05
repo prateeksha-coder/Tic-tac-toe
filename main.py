23b6d1c0ba6793e69f7ef3f8b612f2c251e4ac8e
prateeksha-coder <prateeksha.codingal@gmail.com>
2025-12-05 22:51:31 +0530

    commit

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ init(autoreset=True)
 
 def display_board(board):
     print()
-    print(' ' + board[0] + ' | ' + board[1] + ' | ' + board[2])
+    def colored(cell):
+        if cell== 'X':
+            return Fore.RED +cell+Style.RESET_ALL
+        elif cell== 'O':
+            return Fore.BLUE +cell+Style.RESET_ALL
+        else:
+            return Fore.YELLOW +cell+Style.RESET_ALL
+    print(' ' + colored(board[0]) + ' | ' + colored(board[1]) + ' | ' + colored(board[2]))
     print('———–')
-    print(' ' + board[3] + ' | ' + board[4] + ' | ' + board[5])
+    print(' ' + colored(board[3]) + ' | ' + colored(board[4]) + ' | ' + colored(board[5]))
     print('———–')
-    print(' ' + board[6] + ' | ' + board[7] + ' | ' + board[8])
+    print(' ' + colored(board[6]) + ' | ' + colored(board[7]) + ' | ' + colored(board[8]))
     print()
 def player_choice():
     symbol = ""
